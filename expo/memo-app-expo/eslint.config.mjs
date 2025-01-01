5d5d374cddcde08eb6954384a0e4c234b792749a
tomato378 <mnwtm100@gmail.com>
2025-01-01 17:54:59 +0900

    Create Hello component

--- expo/memo-app-expo/eslint.config.mjs
+++ expo/memo-app-expo/eslint.config.mjs
@@ -1,14 +1,24 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
-
-/** @type {import('eslint').Linter.Config[]} */
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js"
+ 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: globals.node
+    }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+  pluginReactConfig,
+  // rules ここから -----------------------
+  {
+    rules: {
+      "react/react-in-jsx-scope": 0,
+      "semi": [2, "never"],
+      "comma-dangle": [2, "never"]
+    }
+  }
+  // rules ここまで -----------------------
+]
